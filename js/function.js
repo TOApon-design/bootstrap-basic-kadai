9e184ebcb5cd961ecd6051c6be6b0a4d6d789fcd
--global <taot19190721@gmail.com>
2026-09-09 13:11:47 +0900

    課題提出

--- js/function.js
+++ js/function.js
@@ -12,3 +12,30 @@ const sayGoodEvening = () => {
 sayGoodMorning();
 
 sayGoodEvening();
+
+const calculateTotal = (price) => {
+    console.log(price + 500 + '円');
+}
+
+calculateTotal(1200);
+
+const addTwoArgument = (price, shippingFree) => {
+    console.log(price + shippingFree + '円');
+}
+
+addTwoArgument(1200, 700);
+
+const double = (num) => {
+    return num * 2;
+}
+
+console.log(double(30));
+
+const useVariable = () => {
+    const userNames = '侍太郎';
+    console.log(userNames);
+}
+
+useVariable();
+
+console.log(userNames);
---
+++ js/kadai_014/square.js
@@ -0,0 +1,5 @@
+const Argument = (num) => {
+    return num ** 2;
+}
+
+console.log(Argument(10));
